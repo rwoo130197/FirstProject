428a4d6750bc382202f2b37169ad82b8e0418980
1409529 <0>
2016-10-17 14:23:15 +0100

    0

---
+++ Week4/helloPrinter.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charest="UTF-8"
+
+</head>
+
+<body>
+<?php
+$planet = $_Get[]
+?>
+</body>
+
+</html>
---
+++ Week4/helloSelector.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charest="UTF-8"
+
+</head>
+
+<body>
+
+<h2>Hello World</h2>
+
+<p>Where would you like to say hello</p><br>
+
+
+
+<a href="helloPrinter.php?planet=earth ">Earth</a>
+<a href="helloPrinter.php?planet=mars ">Mars</a>
+<a href="helloPrinter.php?planet=uranus ">Uranus</a>
+
+<?php
+
+
+?>
+
+
+</body>
+</html>
+
